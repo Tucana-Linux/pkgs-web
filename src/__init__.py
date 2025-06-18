f3374fbe1d610922f4ca93140d43a29b48c9a28f
Rahul Chandra <rahul@projecteclipse.org>
2025-06-18 03:00:54 -0400

    Add html

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+import requests
+import jinja2
+from neptune.classes.Package import Package
+
+class PkgsWeb:
+    def __init__(repo_url: str, www_dir: str):
+    def get_packages(self):
+    def generate_package_page(self, package: Package) -> None:
+    def generate_home_page(self) -> None:
+    def generate_www_dir(self) -> None:
+        
+        
+
+    
